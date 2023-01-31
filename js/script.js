0e51da973ac5f8ddfd86e425ad428083dfb32436
Duvi <evan.duvivier@icloud.com>
2023-02-01 00:39:51 +0100

    ERSCORD_v0.2

--- js/script.js
+++ js/script.js
@@ -28,7 +28,14 @@ export const sendAMessage = () => {
 	input.addEventListener("keydown", function(e) {
 		if (input.value.length >=1) {
 			if (e.key == "Enter") {
-				chat.innerHTML += '<div class="message"><div class="user-sending"><div class="element mes"></div><div class="user"><p class="username dev">[ZDSK] Ersenzkii</p><p class="username message-sent">'+ input.value + '</p></div></div></div>'
+				chat.innerHTML += '<div class="message"><div class="user-sending"><div class="element mes"></div><div class="user"><p class="username chief">[ZDSK] Ersenzkii</p><p class="username message-sent">'+ input.value + '</p></div></div></div>'
+				input.value = ""
+			} else if (e.key == "AltGraph") {
+				chat.innerHTML += '<div class="message"><div class="user-sending"><div class="element mes"></div><div class="user"><p class="username game">[ZDSK] SpeedRabbit69</p><p class="username message-sent">'+ input.value + '</p></div></div></div>'
+				input.value = ""
+			} else if (e.key == "Escape") {
+				chat.innerHTML += '<div class="message"><div class="user-sending"><div class="element mes"></div><div class="user"><p class="username chief">[ZDSK] ZOmegaZZ</p><p class="username message-sent">'+ input.value + '</p></div></div></div>'
+				input.value = ""
 				input.value = ""
 			}
 		}
